ab353fdc86707d361c42021e916dacc5998b300b
Márton Kardos <power.up1163@gmail.com>
2023-03-21 14:48:47 +0100

    Added __init__.py

---
+++ embedding_explorer/__init__.py
@@ -0,0 +1,4 @@
+from embedding_explorer.app import (get_dash_app, inspect_model,
+                                    show_embedding_dashboard)
+
+__all__ = ["get_dash_app", "inspect_model", "show_embedding_dashboard"]
