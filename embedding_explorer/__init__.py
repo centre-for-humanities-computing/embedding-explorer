1200000bd333d840db41598daf39b96033401cfc
Márton Kardos <power.up1163@gmail.com>
2023-11-13 16:28:37 +0100

    Fixed stupid error with imports

--- embedding_explorer/__init__.py
+++ embedding_explorer/__init__.py
@@ -1,4 +1,10 @@
-from embedding_explorer.app import show_dashboard, show_network_explorer
+from embedding_explorer.app import (show_clustering, show_dashboard,
+                                    show_network_explorer)
 from embedding_explorer.cards import Card
 
-__all__ = ["show_network_explorer", "show_dashboard", "Card"]
+__all__ = [
+    "show_network_explorer",
+    "show_dashboard",
+    "Card",
+    "show_clustering",
+]
